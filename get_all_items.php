ac3bc5456a110262a72d0d67ba9ddb58d2899f6b
datsiii <dasha.tsivina@gmail.com>
2024-08-13 22:19:30 +0300

    first commit

---
+++ get_all_items.php
@@ -0,0 +1,30 @@
+<?php
+
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_name = "db_one";
+$table_name = "users";
+
+$connect = new mysqli($server_name, $user_name, $password, $db_name);
+
+if($connect->connect_error){
+    die("Connection failed: " . $connect->connect_error);
+}
+
+$sql = "SELECT * FROM $table_name";
+$result = $connect->query($sql);
+
+if($result->num_rows > 0){
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        $data[] = $row;
+    }
+    echo json_encode($data);
+}
+else{
+    echo json_encode(array("message" => "Empty List"));
+}
+
+$connect->close();
+?>
